d77dbe971f6369685462a36e88e22c0bd2ea00aa
Roberto Faga <faga@caule.net>
2013-09-25 17:22:00 -0300

    step2 - Authenticating with Django session

--- tastybone/balance/api.py
+++ tastybone/balance/api.py
@@ -1,8 +1,19 @@
 # coding: UTF-8
 
 from tastypie.resources import ModelResource
+from tastypie.authentication import Authentication
 
 from models import *
+
+class DjangoAuthentication(Authentication):
+    def is_authenticated(self, request, **kwargs):
+        if getattr(request, 'user', False) and request.user.is_authenticated():
+            return True
+        return False 
+
+    def get_identifier(self, request):
+        return request.user.username
+
 
 class OperationResource(ModelResource):
     class Meta:
@@ -10,3 +21,4 @@ class OperationResource(ModelResource):
         detail_allowed_methods = ['get', 'post', 'put', 'delete']
         resource_name = 'operation'
         queryset = Operation.objects.all()
+        authentication = DjangoAuthentication()
